6db367d91b3e2c3dd72a8ef18354fb3668b9f1f7
Douglas Araujo <dhoouglas@hotmail.com>
2022-11-08 12:08:08 -0300

    Project Created

---
+++ backend/src/test/java/com/devdouglas/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdouglas.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
