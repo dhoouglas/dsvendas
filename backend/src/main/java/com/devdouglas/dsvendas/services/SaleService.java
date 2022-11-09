a8e9f23e45100a87e0a2d2e123e0ddbf906a8b2c
Douglas Araujo <dhoouglas@hotmail.com>
2022-11-09 16:16:31 -0300

    Group by search

--- backend/src/main/java/com/devdouglas/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devdouglas/dsvendas/services/SaleService.java
@@ -2,6 +2,8 @@ package com.devdouglas.dsvendas.services;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devdouglas.dsvendas.dto.SaleDTO;
+import com.devdouglas.dsvendas.dto.SaleSuccessDTO;
+import com.devdouglas.dsvendas.dto.SaleSumDTO;
 import com.devdouglas.dsvendas.entities.Sale;
 import com.devdouglas.dsvendas.repositories.SaleRepository;
 import com.devdouglas.dsvendas.repositories.SellerRepository;
@@ -27,5 +31,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupBySeller() {
+		return repository.amountGroupBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupBySeller() {
+		return repository.successGroupBySeller();
 	}
 }
