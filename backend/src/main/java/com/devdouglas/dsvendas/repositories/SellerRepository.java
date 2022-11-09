99780eb95b15cadf49e8f9b3957a73b4b99dc1b2
Douglas Araujo <dhoouglas@hotmail.com>
2022-11-09 15:11:23 -0300

    Layers

---
+++ backend/src/main/java/com/devdouglas/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devdouglas.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devdouglas.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
